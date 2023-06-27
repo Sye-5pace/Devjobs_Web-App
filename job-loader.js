ad5566f4cda0d5dc38075e91ec254ef6480cd7a8
Sye-5pace <owusilasyp00@gmail.com>
2023-06-26 22:04:39 -0700

    Redirect functionality created and jobs-detail templatein-progress

--- job-loader.js
+++ job-loader.js
@@ -19,7 +19,7 @@ const createJobCards = (job,index)=>{
         const jobCard = document.createElement('div');
         jobCard.setAttribute('data-index',index)
 
-        const jobIcon = document.createElement('div')
+        const jobIcon = document.createElement('img')
         jobIcon.innerHTML = job.logo
         
         const postedContractTime = document.createElement('h3')
@@ -51,8 +51,47 @@ const createJobCards = (job,index)=>{
 
         
         jobCard.addEventListener('click',(event)=>{
-            const jobIndex = event.currentTarget.dataset.index
+            const clickedJob = event.currentTarget
+            const jobIndex = clickedJob.dataset.index
             console.log("Job #:",jobIndex)
+
+            const companyLogo = job.logo
+            const companyName = job.company
+            const postTimeContract = job.postedAt + '<div class="w-2 h-2 bg-[#6f809b] rounded-full"></div>' + job.contract;
+            const jobRole = job.role
+            const location = job.location
+            const jobDesc = job.desc
+            const requirementTitle = job.requirements.reqTitle
+            const requirementPoint1 = job.requirements.reqItem[0]
+            const requirementPoint2 = job.requirements.reqItem[1]
+            const requirementPoint3 = job.requirements.reqItem[2]
+            const requirementPoint4 = job.requirements.reqItem[3]
+
+            const responsibilityTitle = job.responsibility.resTitle
+            const responsibilityPoint1 = job.responsibility.resItem[0]
+            const responsibilityPoint2 = job.responsibility.resItem[1]
+            const responsibilityPoint3 = job.responsibility.resItem[2]
+            const responsibilityPoint4 = job.responsibility.resItem[3]
+
+            const url = "jobs-detail.html?" + 
+            "&redirectLogo=" + encodeURIComponent(companyLogo) +
+            "&redirectName=" + encodeURIComponent(companyName) +
+            "&redirectTimeContract=" + encodeURIComponent(postTimeContract) +
+            "&redirectRole=" + encodeURIComponent(jobRole) +
+            "&redirectLocation=" + encodeURIComponent(location) +
+            "&redirectDesc=" + encodeURIComponent(jobDesc) +
+            "&redirectReqTitle=" + encodeURIComponent(requirementTitle) +
+            "&redirectReqPt1=" + encodeURIComponent(requirementPoint1) +
+            "&redirectReqPt2=" + encodeURIComponent(requirementPoint2) +
+            "&redirectReqPt3=" + encodeURIComponent(requirementPoint3) +
+            "&redirectReqPt4=" + encodeURIComponent(requirementPoint4) +
+            "&redirectResTitle=" + encodeURIComponent(responsibilityTitle) +
+            "&redirectResPt1=" + encodeURIComponent(responsibilityPoint1) +
+            "&redirectResPt2=" + encodeURIComponent(responsibilityPoint2) +
+            "&redirectResPt3=" + encodeURIComponent(responsibilityPoint3) +
+            "&redirectResPt4=" + encodeURIComponent(responsibilityPoint4);
+            window.location.href = url;
+            // console.log(url)
         })
         return jobCard;
 }
