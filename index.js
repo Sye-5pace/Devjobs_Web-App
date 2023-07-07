e7bdc115238cded33775714b52e14b5c6fec4263
Sye-5pace <owusilasyp00@gmail.com>
2023-07-07 10:39:09 +0100

    theme switcher feature in-progress and switch animation created

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { jobsLoader} from './job-loader.js'
 import { filterByTitle,filterByLocation,sortAll } from './query-helper.js';
+// import { loadTheme,toggleTheme} from "./theme_switcher.js";
 
 document.addEventListener('DOMContentLoaded', ()=>{
     jobsLoader();
 
+    // loadTheme();
     //default refresher for the page
     const defaultRefresher = document.getElementById('default-refresher');
     defaultRefresher.addEventListener('click',()=>{
@@ -12,6 +14,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
         jobsLoader();
     })
 
+    //Sort,Filter features
+    //Sort,Filter features
     const search = document.querySelector('#search');
     search.addEventListener("click", filterByTitle)
 
@@ -36,6 +40,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
         filterMobileModal.style.display = 'none';
     });
 
+    //Theme event listeners
+    //Dark mode
     const darkMode = document.querySelector('#dark');
-    darkMode.addEventListener("click", ()=>{})
+    const switchMode = document.querySelector('#switch');
+    darkMode.addEventListener("click", ()=>{
+        switchMode.classList.add("switch-dark");
+        // toggleTheme(true);
+        console.log("Dark mode");
+    })
+    
+    //Light mode
+    const lightMode = document.querySelector('#light');
+    lightMode.addEventListener("click", ()=>{
+        switchMode.classList.remove("switch-dark");
+        console.log("light mode");
+    })
 })
---
+++ theme_switcher.js
@@ -0,0 +1,59 @@
+//Theme-Switcher EventListener:
+//Persisting the theme with LocalStorage
+export const saveTheme = (darkModeEnabled) => {
+    localStorage.setItem('darkModeEnabled', darkModeEnabled)
+}
+
+
+//Load theme from localStorage:
+export const loadTheme = (darkModeEnabled)=> {
+    const darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+    toggleTheme(darkModeEnabled)
+}
+
+
+export const toggleTheme = (darkModeEnabled)=>{
+    const navbar = document.querySelector('nav');
+    const body = document.querySelector('body');
+    const jobsCards = document.querySelectorAll('.job-card');
+    const sortTitle = document.querySelector('#sort-title');
+    const sortLocation = document.querySelector('#sort-location');
+    const jobs = Array.from(jobsCards)
+    const fullTimeLabel = document.querySelector('#fulltime-label');
+    // const switchMode = document.getElementById('switch');
+
+    if(darkModeEnabled) {
+        //css transition/animation for the mode-node/icon
+        switchMode.classList.add("");
+        body.style.backgroundColor= '#131621';
+        navbar.style.backgroundColor= '#19212c';
+        fullTimeLabel.style.color='#fffffc'
+        sortTitle.style.backgroundColor= '#19212c';
+        sortTitle.style.color= '#fffffc';
+        sortLocation.style.backgroundColor= '#19212c';
+        sortLocation.style.color= '#fffffc';
+        
+        jobs.forEach((jobCard)=>{
+            jobCard.style.backgroundColor= "#19212c";
+            const role = jobCard.querySelector('h2');
+            role.style.color = "#fffffc";
+        })
+        saveTheme(true);
+    }else{
+        switchMode.classList.add("");
+        body.style.backgroundColor= '#f4f6f8';
+        navbar.style.backgroundColor= '#fff';
+        fullTimeLabel.style.color='#6e8096'
+        sortTitle.style.backgroundColor= '#fff';
+        sortTitle.style.color= '#6e8096';
+        sortLocation.style.backgroundColor= '#fff';
+        sortLocation.style.color= '#6e8096';
+
+        jobs.forEach((jobCard)=>{
+            jobCard.style.backgroundColor= "#fff";
+            const role = jobCard.querySelector('h2');
+            role.style.color = "#121721";
+        })
+        saveTheme(false);
+    }
+}
